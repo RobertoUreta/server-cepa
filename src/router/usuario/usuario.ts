3aea241062858b441cc9e61839394e2c42b93cc6
Roberto <roberto.ureta.14@gmail.com>
2019-03-04 11:16:08 -0300

    rol_usuario

--- src/router/usuario/usuario.ts
+++ src/router/usuario/usuario.ts
@@ -1,24 +1,44 @@
-import { Router, Request, Response} from 'express';
+import { Router, Request, Response } from 'express';
 import MySQL from '../../mysql/mysql';
 const cors = require('cors');
 const usuario = Router();
-usuario.use(cors({origin: 'http://localhost:3000'}));
+usuario.use(cors({ origin: 'http://localhost:3000' }));
 
-usuario.get('/usuario', (req: Request, res:Response)=>{
+usuario.get('/usuario', (req: Request, res: Response) => {
     const query = `
-        SELECT id_usuario,nombre
+        SELECT id_usuario,nombre,apellido_paterno,apellido_materno
         FROM usuario
     `;
-    MySQL.ejecutarQuery(query,(err:any,usuarios:Object[])=>{
+    MySQL.ejecutarQuery(query, (err: any, usuarios: Object[]) => {
         if (err) {
             res.status(400).json({
                 ok: false,
                 error: err
             });
-        }else{
+        } else {
+            console.log(usuarios);
             res.json({
                 ok: true,
                 usuarios
+            });
+        }
+    });
+});
+usuario.get('/rol_usuario', (req: Request, res: Response) => {
+    const query = `
+        SELECT id_rol_usuario, nombre_rol
+        FROM rol_usuario
+    `;
+    MySQL.ejecutarQuery(query, (err: any, roles: Object[]) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                error: err
+            });
+        } else {
+            res.json({
+                ok: true,
+                roles
             });
         }
     });
