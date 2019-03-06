e1e642c9c3eb5119766a26271d7ec776366801b3
Unknown <roberto.ureta.14@gmail.com>
2019-03-06 18:02:20 -0300

    sesion

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,24 @@ import usuario from './router/usuario/usuario'
 import express from 'express'
 import session from 'express-session'
 import ingreso from './router/ingreso/ingreso'
-
+import sesion from './router/sesion'
+const cors = require('cors')
 const server = Server.init(3001);
 server.app.use(express.urlencoded({ extended: true }));
 server.app.use(express.json());
-server.app.use(session({secret:'123456'}));
+server.app.use(cors({
+    origin:['http://localhost:3000'],//front url
+    methods:['GET','POST'],
+    credentials: true}));
+server.app.use(session({
+    secret:'123456',
+    saveUninitialized:true,
+    resave:true}));
 server.app.use(router);
 server.app.use(usuario);
 server.app.use(ingreso)
+server.app.use(sesion);
+
 
 server.start(()=>{
     console.log('Servidor corriendo en el puerto 3001');
