7b4c27920049a7e04eb7f2bed427ba68f0150203
Unknown <roberto.ureta.14@gmail.com>
2019-03-08 17:04:25 -0300

    ahora se aplica un hash a las contraseñas

--- src/router/sesion.ts
+++ src/router/sesion.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import MySQL from '../mysql/mysql';
+const bkfd2Password = require('pbkdf2-password');
 const sesion = Router();
+const hasher = bkfd2Password();
 
 sesion.get('/logout', function (req, res) {
     // destroy the user's session to log them out
     // will be re-created next request
-    
+
     req.session!.destroy(function () {
         return res.json({
             ok: true,
@@ -14,33 +16,33 @@ sesion.get('/logout', function (req, res) {
     });
 });
 
-export function restrict(req:Request, res:Response, next:NextFunction) {
+export function restrict(req: Request, res: Response, next: NextFunction) {
     if (req.session!.user) {
         console.log(req.session!.user);
-      next();
+        next();
     } else {
-      req.session!.error = 'Access denied!';
-      return res.json({
-          ok:false,
-          mensaje: 'restrict failed'
-      })
+        req.session!.error = 'Access denied!';
+        return res.json({
+            ok: false,
+            mensaje: 'restrict failed'
+        })
     }
-  }
-  
+}
 
 
 
-  sesion.get('/auth', restrict, function(req, res){
+
+sesion.get('/auth', restrict, function (req, res) {
     res.json({
         ok: true,
         mensaje: 'Auth Correcto'
     })
-  });
+});
+
 
 
 sesion.post('/login', (req: Request, res: Response) => {
     console.log('--login--');
-    //console.log(req.body);
     let pw = req.body.password;
     if (!req.body.usuario || !req.body.password) {
         res.json({
@@ -49,7 +51,7 @@ sesion.post('/login', (req: Request, res: Response) => {
         })
     }
     const query = `
-        SELECT id_usuario, username, password
+        SELECT id_usuario, username, password, salt
         FROM usuario
         WHERE username='${req.body.usuario}'
     `;
@@ -61,23 +63,32 @@ sesion.post('/login', (req: Request, res: Response) => {
             });
         } else {
             let ses = JSON.parse(JSON.stringify(sesion[0]));
-            if (pw === ses.password) {
-                console.log('--MATCH--');
-                req.session!.regenerate(function () {
-                    req.session!.user = sesion[0]
-                    return res.json({
-                        ok: true,
-                        usuario: sesion[0]
+            let opts = {
+                password: req.body.password,//password ingresada por el usuario.
+                salt: ses.salt//Salt asociado al usuario obtenido de la BD.
+            }
+            //Se pasan estas opts para hashear el password ingresado por el usuario y revisarlo con el hash que se encuentra en la BD.
+            hasher(opts, function (err: Error, pass: String, salt: string, hash: string) {
+                if (err) console.log(err);
+                if (hash===ses.password) {
+                    console.log('--MATCH--');
+                    req.session!.regenerate(function () {
+                        req.session!.user = sesion[0]
+                        return res.json({
+                            ok: true,
+                            usuario: sesion[0]
+                        });
                     });
-                });
-            }
-            else {
-                console.log('--PASS INCORRECTA--');
-                return res.json({
-                    ok: false,
-                    mensaje: 'Contraseña incorrecta :('
-                })
-            }
+                }
+                else {
+                    console.log('--PASS INCORRECTA--');
+                    return res.json({
+                        ok: false,
+                        mensaje: 'Contraseña incorrecta :('
+                    })
+                }
+            });
+            
         }
     });
 });
