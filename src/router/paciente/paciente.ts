53245bf175e781ce03283544cbe1ad2bba5e4969
Raimundo Vasquez <rvasquez15@alumnos.utalca.cl>
2019-03-08 15:26:28 -0300

    Cambio query pacientes

--- src/router/paciente/paciente.ts
+++ src/router/paciente/paciente.ts
@@ -5,7 +5,7 @@ const paciente = Router();
 
 paciente.get('/listaPacientes', restrict,(req: Request, res: Response) => {
     const query = `
-        SELECT nombre,apellido_paterno,apellido_materno,rut
+        SELECT id_paciente,nombre,apellido_paterno,apellido_materno,rut
         FROM paciente
         WHERE id_paciente != 1
     `;
@@ -28,7 +28,7 @@ paciente.get('/listaPacientes', restrict,(req: Request, res: Response) => {
 paciente.get('/busquedaPacientes', restrict,(req: Request, res: Response) => {
     console.log(req)
     const query = `
-        SELECT nombre,apellido_paterno,apellido_materno,rut
+        SELECT id_paciente,nombre,apellido_paterno,apellido_materno,rut
         FROM paciente
         WHERE id_paciente != 1 AND CONCAT(nombre,' ',apellido_paterno) LIKE '%${req.query.search}%'
     `;
@@ -48,4 +48,26 @@ paciente.get('/busquedaPacientes', restrict,(req: Request, res: Response) => {
     });
 });
 
+paciente.get('/datosPaciente', restrict, (req: Request,res: Response) => {
+    const query = `
+     SELECT * 
+     FROM paciente
+     WHERE id_paciente = ${req.query.idPaciente}
+    `
+
+    MySQL.ejecutarQuery(query, (err: any, paciente: Object[]) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                error: err
+            });
+        } else {
+            console.log(paciente);
+            res.json({
+                ok: true,
+                paciente
+            });
+        }
+    });
+})
 export default paciente;
