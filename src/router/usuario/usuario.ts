a86c59fce4d80438fcc7b1449e814b6026ad31fc
Unknown <roberto.ureta.14@gmail.com>
2019-03-19 09:08:47 -0300

    API para editar usuario y modificar contraseña

--- src/router/usuario/usuario.ts
+++ src/router/usuario/usuario.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, response } from 'express';
 import MySQL from '../../mysql/mysql';
 import { restrict } from '../sesion';
 const bkfd2Password = require('pbkdf2-password');
@@ -28,6 +28,72 @@ usuario.get('/usuario', restrict, (req: Request, res: Response) => {
     });
 });
 
+function obtenerSupervisor(idUsuario: Number, callback: Function) {
+    let query = `SELECT nombre,apellido_paterno FROM usuario WHERE id_usuario=${idUsuario};`
+    MySQL.ejecutarQuery(query, (err: any, respuesta: Object[]) => {
+        if (err) {
+            return callback(err);
+        }
+        return callback(null, respuesta[0]);
+    });
+}
+function obtenerRolAsociado(idRol: Number, callback: Function) {
+    let query = `SELECT nombre_rol FROM rol_usuario WHERE id_rol_usuario=${idRol};`
+    MySQL.ejecutarQuery(query, (err: any, respuesta: Object[]) => {
+        if (err) {
+            return callback(err);
+        }
+        return callback(null, respuesta[0]);
+    });
+}
+usuario.get('/obtenerUsuario', (req: Request, res: Response) => {
+    var id = req.query.idUser;
+    console.log("obtenerUsuario", req.query)
+    const query = `SELECT *
+                    FROM usuario
+                    WHERE id_usuario=${id}`
+    MySQL.ejecutarQuery(query, (err: any, respuesta: Object[]) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                error: err
+            });
+        } else {
+            let rolID = JSON.parse(JSON.stringify(respuesta[0])).ref_rol;
+            let idSupervisor = JSON.parse(JSON.stringify(respuesta[0])).ref_supervisor;
+            obtenerRolAsociado(rolID, (err: any, rol: Object[]) => {
+                let nombreRol = JSON.parse(JSON.stringify(rol)).nombre_rol;
+                if (idSupervisor !== null) {
+                    obtenerSupervisor(idSupervisor, (err: any, resp: Object[]) => {
+                        if (err) {
+                            return res.json({
+                                ok: false,
+                                error: err
+                            });
+                        }
+                        else {
+                            let supervisor = JSON.parse(JSON.stringify(resp));
+                            res.json({
+                                ok: true,
+                                respuesta,
+                                nombreRol,
+                                supervisor
+                            });
+                        }
+                    });
+                }
+                else {
+                    res.json({
+                        ok: true,
+                        respuesta,
+                        nombreRol
+                    });
+                }
+
+            });
+        }
+    });
+})
 usuario.get('/datosUsuario', (req: Request, res: Response) => {
     var id = req.query.idUser;
     console.log("datosUsuario", req.query)
@@ -94,6 +160,39 @@ usuario.get('/rol_usuario', restrict, (req: Request, res: Response) => {
 });
 
 
+usuario.put('/update_usuario', restrict, (req: Request, res: Response) => {
+    let obj = req.body.data;
+    let idUsuario = req.body.idUsuario;
+    if (obj.supervisorID === "") {
+        console.log("if")
+        obj.supervisorID = null;
+    }
+    if (obj.horasSemanales === "") {
+        obj.horasSemanales = 0
+    }
+    console.log(obj);
+    let estado = 1;
+
+    let query = `UPDATE usuario SET nombre='${obj.nombre}',apellido_paterno='${obj.apellidoPaterno}',apellido_materno='${obj.apellidoMaterno}',
+                                    rut='${obj.rut}',genero='${obj.genero}',username='${obj.usuario}',
+                                    color='${obj.color}',telefono_trabajo='${obj.telefonoTrabajo}',
+                                    telefono_movil='${obj.telefonoMovil}',correo='${obj.correo}',horas_semanales=${obj.horasSemanales},
+                                    nombre_contacto_emergencia='${obj.nombreContactoEmergencia}',telefono_contacto_emergencia='${obj.telefonoContactoEmergencia}',
+                                    estado=${estado},ref_rol=${obj.rolID},ref_supervisor=${obj.supervisorID} WHERE id_usuario=${idUsuario}`;
+    console.log(query)
+    MySQL.ejecutarQuery(query, (err: any, respuesta: Object[]) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        res.status(201).json({
+            ok: true,
+            respuesta
+        });
+    });
+});
 
 usuario.post('/insertar_usuario', restrict, (req: Request, res: Response) => {
     let obj = req.body;
@@ -129,5 +228,64 @@ usuario.post('/insertar_usuario', restrict, (req: Request, res: Response) => {
     });
 
 
+});
+
+
+
+usuario.put('/update_password', (req: Request, res: Response) => {
+    const query = `
+        SELECT password, salt
+        FROM usuario
+        WHERE id_usuario='${req.body.idUsuario}'
+    `;
+    MySQL.ejecutarQuery(query, (err: any, sesion: Object[]) => {
+        if (err) {
+            res.json({
+                ok: false,
+                error: err
+            });
+        } else {
+            console.log(req.body);
+            let ses = JSON.parse(JSON.stringify(sesion[0]));
+            let opts = {
+                password: req.body.data.password,//password ingresada por el usuario.
+                salt: ses.salt//Salt asociado al usuario obtenido de la BD.
+            }
+            //Se pasan estas opts para hashear el password ingresado por el usuario y revisarlo con el hash que se encuentra en la BD.
+            hasher(opts, function (err: Error, pass: String, salt: string, hash: string) {
+                if (err) console.log(err);
+                if (hash===ses.password) {
+                    let opts = {
+                        password: req.body.data.newPassword
+                    }
+                    hasher(opts, function (err: Error, pass: String, salt: string, hash: string) {
+                        if (err) throw err;
+                        // store the salt & hash in the "db"
+                        let query2 = `UPDATE usuario SET password='${hash}', salt='${salt}' WHERE id_usuario=${req.body.idUsuario}`
+                        MySQL.ejecutarQuery(query2, (err: any, respuesta: Object[]) => {
+                            if (err) {
+                                return res.status(500).json({
+                                    ok: false,
+                                    err
+                                });
+                            }
+                            res.status(201).json({
+                                ok: true,
+                                mensaje: "Contraseña cambiada correctamente!"
+                            });
+                        });
+                    });
+                }
+                else {
+                    console.log('--PASS INCORRECTA--');
+                    return res.json({
+                        ok: false,
+                        mensaje: 'Contraseña incorrecta :('
+                    })
+                }
+            });
+            
+        }
+    });
 });
 export default usuario;
